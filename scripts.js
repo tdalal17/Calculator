9835155a07064b9a85ae70a00ecbb40e322650b6
Tanay Dalal <tdalal@hawk.iit.edu>
2023-10-28 23:59:04 -0500

    tipAmount,totalBill,error

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 const billTotalInput = document.getElementById('billTotal');
 const tipSlider = document.getElementById('tipSlider');
 const tipPercentageInput = document.getElementById('tipPercentage');
+const tipAmountInput = document.getElementById('tipAmount');
+const totalBillInput = document.getElementById('totalBill');
+const errorMsg = document.getElementById('error');
