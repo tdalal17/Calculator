99e8b5ae7317b6725dbabcfa7c6f718805a25a14
Tanay Dalal <tdalal@hawk.iit.edu>
2023-10-29 00:03:24 -0500

    added % where we can add depend on up

--- scripts.js
+++ scripts.js
@@ -4,3 +4,22 @@ const tipPercentageInput = document.getElementById('tipPercentage');
 const tipAmountInput = document.getElementById('tipAmount');
 const totalBillInput = document.getElementById('totalBill');
 const errorMsg = document.getElementById('error');
+
+document.getElementById('tipForm').addEventListener('input', function() {
+    const billTotal = parseFloat(billTotalInput.value);
+    const tipPercentage = parseFloat(tipSlider.value);
+
+    if (isNaN(billTotal)) {
+        errorMsg.style.display = 'block';
+        return;
+    } else {
+        errorMsg.style.display = 'none';
+    }
+
+    const tipAmount = (billTotal * tipPercentage / 100).toFixed(2);
+    const totalBill = (billTotal + parseFloat(tipAmount)).toFixed(2);
+
+    tipPercentageInput.value = tipPercentage + '%';
+    tipAmountInput.value = tipAmount;
+    totalBillInput.value = totalBill;
+});
